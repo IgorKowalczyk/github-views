30af9337df3bc9aa443ffb631777a4ae6e46df17
Igor Kowalczyk <majonezexe@protonmail.com>
2023-01-25 19:47:38 +0100

    Format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
-import vercel from '@astrojs/vercel/serverless';
-
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
