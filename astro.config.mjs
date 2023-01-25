2139e536e4ee28712d16d217b19b5a02dad26f73
Igor Kowalczyk <majonezexe@protonmail.com>
2023-01-25 18:27:54 +0100

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
  output: "server",
+ site: "https://views.igorkowalczyk.dev",
+ adapter: vercel(),
  integrations: [tailwind(), compress()],
 });
