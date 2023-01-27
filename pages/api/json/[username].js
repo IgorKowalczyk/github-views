ab70be6dc3b8a621932ba21e15eb86e743702572
Igor Kowalczyk <majonezexe@protonmail.com>
2023-01-27 12:00:39 +0100

    Delete old pages

--- pages/api/json/[username].js
+++
@@ -1,9 +0,0 @@
-import { GetViews } from "../../../database/Views";
-
-export default async function handler(req, res) {
- const { username } = req.query;
- res.setHeader("Content-Type", "application/json");
- res.setHeader("Cache-Control", "no-cache");
- if (!username) return res.status(400).send({ error: "No username provided" });
- return res.status(200).send({ views: await GetViews(username), username: username });
-}
