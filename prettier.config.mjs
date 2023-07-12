89c671d5bbb4a0ba8aa352d61bfbdd3e662a37eb
Igor Kowalczyk <majonezexe@protonmail.com>
2023-07-12 11:38:25 +0200

    Update packages

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -4,4 +4,12 @@ import config from "@igorkowalczyk/prettier-config";
 export default {
  ...config,
  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+ overrides: [
+  {
+   files: "*.astro",
+   options: {
+    parser: "astro",
+   },
+  },
+ ],
 };
