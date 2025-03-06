cfe1fc4c4d3c8cedae9d8f1de2c9a9bb1b01581c
Igor Kowalczyk <majonezexe@protonmail.com>
2025-03-06 19:51:31 +0100

    Update to Tailwind CSS v4

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,3 @@
 import { forwardRef, type InputHTMLAttributes } from "preact/compat";
 
-export const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>((props, ref) => <input ref={ref} class=" w-full max-w-md rounded-lg border border-neutral-800 bg-transparent p-2 font-normal text-white outline-none duration-200 focus:border-neutral-700" {...props} />);
+export const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>((props, ref) => <input ref={ref} class=" w-full rounded-lg border border-neutral-800 bg-transparent p-2 font-normal text-white outline-hidden duration-200 focus:border-neutral-700" {...props} />);
