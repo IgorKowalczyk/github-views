b421cf5db8e50936739e0819ab6d87f9f8f92b59
Igor Kowalczyk <majonezexe@protonmail.com>
2025-03-20 19:06:06 +0100

    Remove appEntrypoint specification from Vue integration in Astro config

--- astro.config.ts
+++ astro.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
  },
  output: "server",
  adapter: deno(),
- integrations: [vue({ appEntrypoint: "/src/pages/" }), compress()],
+ integrations: [vue(), compress()],
  vite: {
   plugins: [tailwindcss()],
  },
