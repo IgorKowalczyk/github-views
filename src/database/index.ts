38ac7b7845f353c6e913fbe4d86d7fe41a76f38b
Igor Kowalczyk <majonezexe@protonmail.com>
2025-03-22 17:01:24 +0100

    Prevent view count increment for specific usernames

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,12 +3,13 @@ import "./patch";
 export async function increaseViews(username: string, env: Cloudflare.Env): Promise<number> {
  if (username === "example") return 0;
 
+ // No views for thebabayaga or thebabayagakiller, don't spam the api bro
+ if (username === "thebabayaga" || username === "thebabayagakiller") return 0;
+
  const kv = env.views;
  if (!kv) throw new Error("KV Namespace not available! Check your configuration - The KV namespace should be named 'views'.");
 
  const currentViews = Number((await kv.get(username)) || 0);
-
- // Increase views by 1
  const newViews = currentViews + 1;
  await kv.put(username, newViews.toString());
 
