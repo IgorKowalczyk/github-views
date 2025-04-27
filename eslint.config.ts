3f6cb86f655d99f6f36f328664048c8882347563
Igor Kowalczyk <majonezexe@protonmail.com>
2025-04-27 15:01:07 +0200

    Update packages

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,12 @@
 import eslintConfig from "@igorkowalczyk/eslint-config";
+import { defineConfig } from "eslint/config";
 import eslintPluginAstro from "eslint-plugin-astro";
 // import pluginVue from "eslint-plugin-vue";
 
-export default [
- // prettier
- ...eslintConfig.base,
- ...eslintConfig.node,
- ...eslintPluginAstro.configs["flat/recommended"],
- // ...pluginVue.configs["flat/recommended"],
- ...eslintConfig.typescript,
-];
+export default defineConfig([
+ eslintConfig.base,
+ eslintConfig.node,
+ eslintPluginAstro.configs["flat/recommended"],
+ // pluginVue.configs["flat/recommended"],
+ eslintConfig.typescript,
+]);
